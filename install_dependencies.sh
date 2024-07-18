7db0aa8a2d358a2c90ab772a025291def54fc8a6
Shantanu Singh <shantanusingh10@gmail.com>
2024-07-18 17:11:36 +0530

    Fixed requirements and added weight download to dependencies script.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies for the project
-sudo apt install libjpeg-dev zlib1g-dev
+sudo apt install -y libjpeg-dev zlib1g-dev wget
 
 # Install requirements
 pip install -r requirements.txt
@@ -10,8 +10,15 @@ pip install -r requirements.txt
 git submodule update --init --recursive
 
 # Navigate to RepViT submodule and install package
-cd RepViT
-pip install -e .
+sudo chmod -R 777 RepViT
+cd RepViT/sam
+pip install --user -e . 
+
+# Dowload the sam weights using wget
+mkdir weights
+cd weights
+wget https://github.com/THU-MIG/RepViT/releases/download/v1.0/repvit_sam.pt
+
 
 echo "Dependencies installed successfully"
 
